e060a4126acb6e3c6eb82c0f67a37cb525f56cee
ChinaZeng <z.z.w941124@qq.com>
2018-08-26 15:23:52 +0800

    基础ui架构

---
+++ app/src/main/java/com/zzw/socketdemo/ui/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zzw.socketdemo.ui;
+
+import com.zzw.socketdemo.R;
+import com.zzw.socketdemo.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+
+    @Override
+    protected int initLayoutId() {
+        return R.layout.activity_main;
+    }
+}
